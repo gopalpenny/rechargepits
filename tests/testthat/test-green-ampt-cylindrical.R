b210a76ceeb4adc0d347e00c62c3d7da4f52dbe4
Gopal Penny <gopalpenny@gmail.com>
2024-01-01 13:55:55 -0800

    added test for get_greenampt_cyl_horiz_numerical

--- tests/testthat/test-green-ampt-cylindrical.R
+++ tests/testthat/test-green-ampt-cylindrical.R
@@ -25,3 +25,28 @@ test_that("get_greenampt_cyl_horiz_time works", {
 test_that("get_greenampt_cyl_horiz_numerical works", {
   expect_equal(round(F_c_calculated, 5), set_units(c(0, 1, 5, 10, 20), "ft^2"))
 })
+
+r_b <- set_units(2, "ft") # length
+theta_0 <- 0.2 # unitless
+theta_s <- 0.35 # unitless
+times <- set_units(c(0, 0.5, 1, 5), "hr")
+Ksat <- set_units(0.2, "cm/h") # length / time
+h_b <- set_units(4, "ft") # hydraulic head (length)
+h_0 <- set_units(-10, "cm") # hydraulic head (length)
+d <- set_units(3, "ft")
+num_sections <- 3
+F_v_cum <- get_greenampt_cyl_flow_integrated(theta_0, theta_s, Ksat, h_b, h_0, r_b, times, F_units = "ft^2", num_sections = 3, d = d)
+
+# paste(round(F_v_cum, 5), collapse = ", ")
+
+test_that("get_greenampt_cyl_flow_integrated works", {
+  expect_equal(round(F_v_cum, 5), set_units(c(0, 92.47857, 133.63127, 324.4744), "ft^3"))
+})
+
+# # This is equivalent to a discretized version of `get_greenampt_cyl_horiz_numerical`:
+# Fc_1_5 <- get_greenampt_cyl_horiz_numerical(theta_0, theta_s, Ksat, h_b = set_units(1.5,"ft"), h_0, r_b, times, F_units = "ft^2")
+# Fc_2_5 <- get_greenampt_cyl_horiz_numerical(theta_0, theta_s, Ksat, h_b = set_units(2.5,"ft"), h_0, r_b, times, F_units = "ft^2")
+# Fc_3_5 <- get_greenampt_cyl_horiz_numerical(theta_0, theta_s, Ksat, h_b = set_units(3.5,"ft"), h_0, r_b, times, F_units = "ft^2")
+# Fc_sum <- d / num_sections * (Fc_1_5 + Fc_2_5 + Fc_3_5)
+# F_v_cum
+# Fc_sum
